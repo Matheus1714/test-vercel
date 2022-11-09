eb8e9e1490ae2fa85566c735bea7ebfcb10711c3
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-09 01:40:50 -0300

    config to use routes

---
+++ routes.py
@@ -0,0 +1,28 @@
+
+
+def routes(app):
+    @app.route('/')
+    def home():
+        return 'Home Page Route'
+
+
+    @app.route('/about')
+    def about():
+        return 'About Page Route'
+
+
+    @app.route('/portfolio')
+    def portfolio():
+        return 'Portfolio Page Route'
+
+
+    @app.route('/contact')
+    def contact():
+        return 'Contact Page Route'
+
+
+    @app.route('/api')
+    def api():
+        with open('data.json', mode='r') as my_file:
+            text = my_file.read()
+            return text
